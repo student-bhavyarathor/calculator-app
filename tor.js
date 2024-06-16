95d7b000009b858d846089b365cbb278aca1fc84
student-bhavyarathor <rathorbhavya80@gmail.com>
2024-06-16 14:56:52 +0530

    Update tor.js

--- tor.js
+++ tor.js
@@ -4,7 +4,7 @@ let buttons = document.querySelectorAll('button');
 let string = "";
 
 buttons.forEach(button => {
-    button.addEventListener('click', (e) => {aro
+    button.addEventListener('click', (e) => {
         if (e.target.innerHTML === '=') {
             string = eval(string);
             input.value = string;
